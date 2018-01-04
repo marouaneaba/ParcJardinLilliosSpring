27cf7f13da63b454fc129ccf41da911478d7a0bd
elazzam.mustapha@hotmail.fr <xal2llx2a>
2018-01-04 13:36:14 +0100

    template de notre page web admin

--- src/main/java/com/lille1/ParcsJardinsLillios/Web/ParcJardinController.java
+++ src/main/java/com/lille1/ParcsJardinsLillios/Web/ParcJardinController.java
@@ -1,4 +1,24 @@
 package com.lille1.ParcsJardinsLillios.Web;
 
+
+import com.lille1.ParcsJardinsLillios.Service.Interfaces.ParcJardinInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ParcJardinController {
+
+
+    @Autowired
+    private ParcJardinInterface parcJardinInterfaceMetier;
+
+    @RequestMapping(value="/operationPJ")
+    public String index(Model model){
+        return "parcJardin";
+    }
+
+
+
 }
