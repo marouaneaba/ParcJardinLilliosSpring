45c7e4f421764c7bc411fe320099d390cb345476
elazzam.mustapha@hotmail.fr <xal2llx2a>
2018-02-12 00:56:14 +0100

    suppression d un parc fonctionne j ai modifer le service et ajouter deux fonction dans horaire repo

--- src/main/java/com/lille1/ParcsJardinsLillios/DAO/HoraireRepository.java
+++ src/main/java/com/lille1/ParcsJardinsLillios/DAO/HoraireRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lille1.ParcsJardinsLillios.Entity.*;
 
-public interface HoraireRepository extends JpaRepository<Horaire, Long>{
+import java.util.List;
 
+public interface HoraireRepository extends JpaRepository<Horaire, Long>{
+public void delete(Horaire horaire);
+public List<Horaire> findByParcJardin(ParcJardin parcJardin);
 }
