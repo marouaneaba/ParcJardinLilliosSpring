6df11653052d7499abbe4ffc94bd26210c3ece6a
elazzam.mustapha@hotmail.fr <xal2llx2a>
2018-02-11 01:14:11 +0100

    je suis passe de sql a h2

--- src/main/java/com/lille1/ParcsJardinsLillios/Service/Interfaces/ParcJardinInterface.java
+++ src/main/java/com/lille1/ParcsJardinsLillios/Service/Interfaces/ParcJardinInterface.java
@@ -5,60 +5,45 @@ import com.sun.org.omg.CORBA.ParDescriptionSeqHelper;
 
 import java.util.List;
 
-
 public interface ParcJardinInterface {
 
-	 public List<ParcJardin> ConsulterParcsJardin();
-	 public ParcJardin ChercherPJParId(Long id);
+	public List<ParcJardin> ConsulterParcsJardin();
+
+	public ParcJardin ChercherPJParId(Long id);
+
 	public void SupprimerPJ(ParcJardin PJ);
-
-
-
-	 public ParcJardin JardinSet(ParcJardin pj);
-
-	 public List<ParcJardin> ChercherPJParMotCle(String KeyWordJP);
-	
 	public ParcJardin AjouterListCatToPJ(List<Categorie> lcat, ParcJardin PJ);
+	public ParcJardin JardinSet(ParcJardin pj);
 
-	 public ParcJardin chercherPJParNom(String nomPj);
+	public List<ParcJardin> ChercherPJParMotCle(String KeyWordJP);
 
-	 public List<ParcJardin> chercherPJParCategorie(Categorie categorie);
+	public ParcJardin chercherPJParNom(String nomPj);
 
-	 public boolean AjouterPJ(ParcJardin PJ);
-	 
+	public List<ParcJardin> chercherPJParCategorie(Categorie categorie);
 
+	public boolean AjouterPJ(ParcJardin PJ);
 
-	 public ParcJardin ModifierDescriptionPJ(Long idPJ, String desc);
+	public ParcJardin ModifierDescriptionPJ(Long idPJ, String desc);
 
-	 public ParcJardin ModifierNomPJ(Long idPJ, String nom);
+	public ParcJardin ModifierNomPJ(Long idPJ, String nom);
 
-	 //public List<Horaire> ConsulterHorairesPJ(Long idPJ);
-	 public void ModifierHorairesPJ(ParcJardin PJ, List<Horaire> horaire);
+	// public List<Horaire> ConsulterHorairesPJ(Long idPJ);
+	public void ModifierHorairesPJ(ParcJardin PJ, List<Horaire> horaire);
 
-	 //public List<Commentaire> ConsulterCommentairesPJ(ParcJardin PJ);
-	 //public ParcJardin ajouterCommentaireToPJ(Commentaire commentaire , ParcJardin PJ);
-	 //public ParcJardin supprimerCommetaireFromPJ(Commentaire commentire, ParcJardin PJ );
+	// public List<Commentaire> ConsulterCommentairesPJ(ParcJardin PJ);
+	// public ParcJardin ajouterCommentaireToPJ(Commentaire commentaire ,
+	// ParcJardin PJ);
+	// public ParcJardin supprimerCommetaireFromPJ(Commentaire commentire,
+	// ParcJardin PJ );
 
+	public List<Categorie> ConsulterCategoriesPJ(Long idPJ);
 
-	 public List<Categorie> ConsulterCategoriesPJ(Long idPJ);
-	 public ParcJardin ajouterCategorieToPJ(Categorie categorie, ParcJardin PJ);
-	 public ParcJardin supprimerCategorieFromPJ(Categorie categorie, ParcJardin PJ);
+	public ParcJardin ajouterCategorieToPJ(Categorie categorie, ParcJardin PJ);
 
-	 public ParcJardin chercherPJLG(String l , String g);
+	public ParcJardin supprimerCategorieFromPJ(Categorie categorie, ParcJardin PJ);
 
+	public ParcJardin chercherPJLG(String l, String g);
 
 	public List<ParcJardin> consulterPJByType(String type);
-
-
-
-
-
-
-
-
-
-
-	 
-	 
 
 }
